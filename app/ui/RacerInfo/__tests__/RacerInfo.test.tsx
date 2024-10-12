a576688142537f0e0567566f0e7914adea763ca6
Derek McIntire <derekbmcintire@gmail.com>
2024-10-12 08:01:49 -0400

    All tests passing again

--- app/ui/RacerInfo/__tests__/RacerInfo.test.tsx
+++ app/ui/RacerInfo/__tests__/RacerInfo.test.tsx
@@ -1,45 +1,49 @@
-// RacerInfoBlock.test.js
+import React from 'react';
+import { mockRacer, TEAM_B2C2_CONTES } from '../../../mockData/mockRacer';
 
-// import React from 'react';
-// import { buildMockRacerInfo } from '@/app/mockData/generators/racer';
-// import { mockRacer } from '@/app/mockData/mockRacer';
-// import { render, screen } from '../../../../test-utils';
-// import Details from '../components/Details';
-// import { RACER_PROFILE_IMAGE_TEST_ID } from '../components/ProfileImage';
-// import RacerInfoContainer from '../components/RacerInfoContainer';
+import '@testing-library/jest-dom/jest-globals';
+import '@testing-library/jest-dom';
 
-// afterEach(() => {
-//   jest.restoreAllMocks();
-// });
+import { render, screen } from '../../../../test-utils';
+import Details from '../components/Details';
+import { RACER_PROFILE_IMAGE_TEST_ID } from '../components/ProfileImage';
+import RacerInfoContainer from '../components/RacerInfoContainer';
 
-// // use mockRacer instead of randomly generated mocks
-// jest.mock('../../../mockData/generators/racer', () => ({
-//   buildMockRacerInfo: jest.fn(() => mockRacer),
-// }));
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
-// describe('RacerInfoBlock', () => {
-//   test.skip('renders the racer details', () => {
-//     // const { socials, dob, categories, hometown } = mockRacer;
-//     // render(<Details socials={socials} dob={dob} categories={categories} hometown={hometown} />);
-//     // expect(screen.getByText(/USA/i)).toBeInTheDocument();
-//     // expect(screen.getByText(/Dob/i)).toBeInTheDocument();
-//     // expect(screen.getByText(/cx: 3/i)).toBeInTheDocument();
-//   });
-// });
+// use mockRacer instead of randomly generated mocks
+jest.mock('../../../mockData/generators/racer', () => ({
+  buildMockRacerInfo: jest.fn(() => mockRacer),
+}));
 
-// describe('RacerInfoContainer', () => {
-//   test.skip('renders the correct contents', () => {
-//     // render(<RacerInfoContainer />);
-//     // expect(buildMockRacerInfo()).toBe(mockRacer);
-//     // expect(screen.getByText(/Top Results/i)).toBeInTheDocument();
-//     // expect(screen.getByText(/Upcoming Races/i)).toBeInTheDocument();
-//     // expect(screen.getByText(/cx: 3/i)).toBeInTheDocument();
-//     // expect(screen.getByTestId(RACER_PROFILE_IMAGE_TEST_ID)).toBeInTheDocument();
-//   });
-// });
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockRacer),
+  })
+) as jest.Mock;
 
-describe('temp', () => {
-  test('placeholder', () => {
-    expect(true).toBe(true);
+describe('RacerInfoBlock', () => {
+  test('renders the racer details', () => {
+    const { socials, dob, categories, hometown } = mockRacer;
+    render(<Details socials={socials} dob={dob} categories={categories} hometown={hometown} />);
+    expect(screen.getByText(/USA/i)).toBeInTheDocument();
+    expect(screen.getByText(/Dob/i)).toBeInTheDocument();
+    expect(screen.getByText(/cx: 3/i)).toBeInTheDocument();
+  });
+});
+
+describe('RacerInfoContainer', () => {
+  test('renders with mockRiderInfo when fetch is mocked', async () => {
+    const component = await RacerInfoContainer({ id: 2 });
+
+    render(component);
+
+    expect(screen.getByText(TEAM_B2C2_CONTES)).toBeInTheDocument();
+    expect(screen.getByText(/Top Results/i)).toBeInTheDocument();
+    expect(screen.getByText(/Upcoming Races/i)).toBeInTheDocument();
+    expect(screen.getByText(/cx: 3/i)).toBeInTheDocument();
+    expect(screen.getByTestId(RACER_PROFILE_IMAGE_TEST_ID)).toBeInTheDocument();
   });
 });
