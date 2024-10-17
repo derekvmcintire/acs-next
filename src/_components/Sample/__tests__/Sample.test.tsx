c7659d49c288d9afd7df77250dab71b072a2e01c
Derek McIntire <derekbmcintire@gmail.com>
2024-10-17 15:25:58 -0400

    working

--- src/_components/Sample/__tests__/Sample.test.tsx
+++ src/_components/Sample/__tests__/Sample.test.tsx
@@ -1,12 +1,41 @@
+import { mockRacingHistory } from '@/src/_db/mock-data/mock-race-history';
+import { mockRider } from '@/src/_db/mock-data/mock-racer';
+import { mockMultiGlobalFetch } from '@/src/test-helpers';
 import { render, screen } from '@/test-utils';
 import Sample from '../Sample';
+
+const firstExpectedURL = 'http://localhost:8000/history?racerId=1';
+const firstMockResponse = [mockRacingHistory];
+
+const secondExpectedURL = 'http://localhost:8000/racers?id=1';
+const secondMockResponse = [mockRider];
+
+afterEach(() => {
+  // restore all mocks after each test
+  jest.restoreAllMocks();
+});
+
+beforeEach(() => {
+  mockMultiGlobalFetch(
+    [firstExpectedURL, secondExpectedURL],
+    [firstMockResponse, secondMockResponse]
+  );
+});
 
 describe('Sample', () => {
   it('Renders Something', async () => {
     render(<Sample />);
+    // we should initially get a loading state
     expect(screen.getByText(/Loading/i)).toBeInTheDocument();
-    const doneElement = await screen.findByText(/Done/i);
-    expect(doneElement).toBeInTheDocument();
-    expect(screen.getByText(/Done/i)).toBeInTheDocument();
+
+    const historyResult = mockRacingHistory.results[0].year;
+    const riderResult = mockRider.name.first;
+
+    // use await screen.findByText which waits until our mock fetch returns a response
+    const histroyElement = await screen.findByText(historyResult);
+    expect(histroyElement).toBeInTheDocument();
+
+    const riderElement = await screen.findByText(riderResult);
+    expect(riderElement).toBeInTheDocument();
   });
 });
---
+++ src/test-helpers.ts
@@ -0,0 +1,35 @@
+/*
+ ** This function will mock the global fetch object and return however many different responses are necessary
+ */
+export const mockMultiGlobalFetch = (expectedUrls: string[], mockResponses: any[]): void => {
+  global.fetch = jest.fn((input) => {
+    const index = expectedUrls.indexOf(
+      expectedUrls.find((url: string) => url === String(input)) || ''
+    );
+    if (index < 0) {
+      console.log(
+        "Uh oh, couldn't find the expected url, make sure you have passed a URL for each expected fetch request!"
+      );
+    }
+    const mockResponse = mockResponses[index];
+    if (mockResponse.length < index + 1) {
+      console.log(
+        "Uh oh, couldn't find the expected mock response, make sure you have passed a mock response for each expected fetch request!"
+      );
+    }
+    return Promise.resolve({
+      json: () => Promise.resolve(mockResponses[index]),
+    });
+  }) as jest.Mock;
+};
+
+/*
+ ** This function will mock the global fetch object and return a mocked response.
+ */
+export const mockGlobalFetch = (mockResponse: any): void => {
+  global.fetch = jest.fn(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(mockResponse),
+    });
+  }) as jest.Mock;
+};
