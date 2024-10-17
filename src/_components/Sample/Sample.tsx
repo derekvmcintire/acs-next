98e98c0be2d55caaea25c8284553e069ab3348ee
derekvmcintire <77245762+derekvmcintire@users.noreply.github.com>
2024-10-17 15:53:16 -0400

    working (#35)
    * working
    
    * getURL functions
    
    * clean up
    
    ---------
    
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/_components/Sample/Sample.tsx
+++ src/_components/Sample/Sample.tsx
@@ -3,15 +3,19 @@
 import React from 'react';
 import { Text } from '@mantine/core';
 import { getRiderHistory } from '@/src/_api/get-history';
+import { getSingleRider } from '@/src/_api/get-rider';
 
 export default function Sample() {
   const [thing, setThing] = React.useState<any>(null);
+  const [secondThing, setSecondThing] = React.useState<any>(null);
 
   React.useEffect(() => {
     const fetchSomething = async () => {
-      const response = await getRiderHistory(1);
-      console.log(response);
-      setThing('Done');
+      const historyResponse = await getRiderHistory(1);
+      const riderResponse = await getSingleRider(1);
+
+      setThing(`${historyResponse[0].year}`);
+      setSecondThing(`${riderResponse?.name.first}`);
     };
     fetchSomething();
   });
@@ -19,5 +23,10 @@ export default function Sample() {
   if (!thing) {
     return <Text>Loading</Text>;
   }
-  return <Text>{thing}</Text>;
+  return (
+    <>
+      <Text>{thing}</Text>
+      <Text>{secondThing}</Text>
+    </>
+  );
 }
--- src/_components/Sample/__tests__/Sample.test.tsx
+++ src/_components/Sample/__tests__/Sample.test.tsx
@@ -1,12 +1,45 @@
+import { getRiderHistoryRequestUrl } from '@/src/_api/get-history';
+import { getSingleRiderRequestUrl } from '@/src/_api/get-rider';
+import { mockRacingHistory } from '@/src/_db/mock-data/mock-race-history';
+import { mockRider } from '@/src/_db/mock-data/mock-racer';
+import { mockMultiGlobalFetch } from '@/src/test-helpers';
 import { render, screen } from '@/test-utils';
 import Sample from '../Sample';
+
+// set up mock responses for global.fetch
+const firstExpectedURL = getRiderHistoryRequestUrl(1);
+const firstMockResponse = [mockRacingHistory];
+
+const secondExpectedURL = getSingleRiderRequestUrl(1);
+const secondMockResponse = [mockRider];
+
+const expectedUrls = [firstExpectedURL, secondExpectedURL];
+const expectedMockResponses = [firstMockResponse, secondMockResponse];
+
+afterEach(() => {
+  // restore all mocks after each test
+  jest.restoreAllMocks();
+});
+
+beforeEach(() => {
+  // use helper to mock all fetch requests
+  mockMultiGlobalFetch(expectedUrls, expectedMockResponses);
+});
 
 describe('Sample', () => {
   it('Renders Something', async () => {
     render(<Sample />);
+    // we should initially get a loading state
     expect(screen.getByText(/Loading/i)).toBeInTheDocument();
-    const doneElement = await screen.findByText(/Done/i);
-    expect(doneElement).toBeInTheDocument();
-    expect(screen.getByText(/Done/i)).toBeInTheDocument();
+
+    const historyResult = mockRacingHistory.results[0].year;
+    const riderResult = mockRider.name.first;
+
+    // use await screen.findByText which waits until our mock fetch returns a response
+    const histroyElement = await screen.findByText(historyResult);
+    expect(histroyElement).toBeInTheDocument();
+
+    const riderElement = await screen.findByText(riderResult);
+    expect(riderElement).toBeInTheDocument();
   });
 });
