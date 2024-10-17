625cc0ba193430761f7d685db3cfa696e2747c5a
Derek McIntire <derekbmcintire@gmail.com>
2024-10-17 13:09:32 -0400

    working

--- src/_components/Rider/client/TopResults.tsx
+++ src/_components/Rider/client/TopResults.tsx
@@ -1,35 +1,19 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
 import { Text, useMantineColorScheme } from '@mantine/core';
-import { getRiderHistory } from '@/src/_api/get-history';
-import { IRaceData } from '@/src/_types';
+import { IRaceData, IRiderInfo } from '@/src/_types';
 import { getFormattedYearString } from '@/src/_utility/date-helpers';
 import InfoBlock from '../../_ui/InfoBlock';
 import LabeledText from '../../_ui/LabeledText';
-import { getCareerWins, getOrdinal, getTopTenResults } from '../../Results/utils';
+import { getOrdinal } from '../../Results/utils';
 import classes from '../styles/rider.module.css';
 
-interface TopResultsProps {
-  id: number;
+interface MappedResultsProps {
+  topResults: IRaceData[];
 }
 
-export default function TopResults({ id }: TopResultsProps) {
-  const [topResults, setTopResults] = useState<IRaceData[]>([]);
-  const [careerWins, setCareerWins] = useState<number>(0);
-
+const MappedResults = ({ topResults }: MappedResultsProps) => {
   const { colorScheme } = useMantineColorScheme();
-
-  useEffect(() => {
-    const fetchRiderResults = async () => {
-      await getRiderHistory(id).then((data) => {
-        setCareerWins(getCareerWins(data));
-        setTopResults(getTopTenResults(data));
-      });
-    };
-
-    fetchRiderResults();
-  }, []);
 
   const getTopResultPlaceColor = (place: number) => {
     switch (place) {
@@ -45,30 +29,44 @@ export default function TopResults({ id }: TopResultsProps) {
   };
 
   return (
+    <div className={classes.topResultsList}>
+      {topResults.map((result) => (
+        <div key={`${result.startDate}${result.points}`}>
+          <LabeledText
+            size="xs"
+            color={getTopResultPlaceColor(result.place)}
+            label={`${getOrdinal(result.place)}`}
+            text={`at ${result.name} (${getFormattedYearString(new Date(result.startDate))})`}
+            noColon
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+interface WinsProps {
+  wins: number;
+}
+const Wins = ({ wins }: WinsProps) => (
+  <Text size="xs" c="orange" fw={700}>{`${wins} Career Wins`}</Text>
+);
+
+type TopResultsProps = IRiderInfo;
+
+export default function TopResults(riderInfo: TopResultsProps) {
+  const { wins, topResults } = riderInfo;
+
+  const hasWins = wins && wins > 0;
+  const hasTopResults = topResults && topResults.length > 0;
+
+  return (
     <section className={classes.topResults} data-testid="top-results">
-      {topResults.length > 0 ? (
-        <InfoBlock>
-          <Text fw={900}>Top Results</Text>
-          {careerWins > 0 && (
-            <Text size="xs" c="orange" fw={700}>{`${careerWins} Career Wins`}</Text>
-          )}
-          <div className={classes.topResultsList}>
-            {topResults.map((result) => (
-              <div key={`${result.startDate}${result.points}`}>
-                <LabeledText
-                  size="xs"
-                  color={getTopResultPlaceColor(result.place)}
-                  label={`${getOrdinal(result.place)}`}
-                  text={`at ${result.name} (${getFormattedYearString(new Date(result.startDate))})`}
-                  noColon
-                />
-              </div>
-            ))}
-          </div>
-        </InfoBlock>
-      ) : (
-        <Text>Loading...</Text>
-      )}
+      <InfoBlock>
+        <Text fw={900}>Top Results</Text>
+        {hasWins && <Wins wins={wins} />}
+        {hasTopResults ? <MappedResults topResults={topResults} /> : <>No Top Results Found</>}
+      </InfoBlock>
     </section>
   );
 }
