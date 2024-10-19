9c850e7281be5ff87113a6ba985fa10cb265bebe
derekvmcintire <77245762+derekvmcintire@users.noreply.github.com>
2024-10-19 06:46:22 -0400

    Remove photo destructure (#45)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/_components/Rider/client/InfoGrid.tsx
+++ src/_components/Rider/client/InfoGrid.tsx
@@ -13,14 +13,13 @@ const INFO_GRID_TEST_ID = 'info-grid';
 
 export default function InfoGrid() {
   const { riderInfo, riderTeamMembers } = useRider();
-  const { photo } = riderInfo;
 
   return (
     <div className={classes.infoGrid} data-testId={INFO_GRID_TEST_ID}>
       <Grid>
         <Grid.Col span={5}>
           <Flex>
-            <ProfileImage img={photo} />
+            <ProfileImage />
             <Details {...riderInfo} />
           </Flex>
         </Grid.Col>
