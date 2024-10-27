239cc3db7b6e501105a47741bbbd3ed8bf4194c4
Derek McIntire <derekbmcintire@gmail.com>
2024-10-27 13:56:06 -0400

    Working with new API routes

--- src/_components/shared/TopNav/Search.tsx
+++ src/_components/shared/TopNav/Search.tsx
@@ -1,37 +1,66 @@
+'use client';
+
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import { GoSearch } from 'react-icons/go';
 import { Autocomplete } from '@mantine/core';
 import { getRidersByName } from '@/src/_api/get-riders-by-name';
 import { IGetRidersResponse } from '@/src/_api/types';
+import useDebounce from '@/src/_hooks/use-debounce';
 import { IRiderInfo } from '@/src/_types';
+import { APP_BASE_URL, APP_RIDER_PATH } from '@/src/global-constants';
 
 const icon = <GoSearch />;
 
-export default function Search() {
-  const [value, setValue] = React.useState('');
-  const [data, setData] = React.useState<string[]>([]);
+type SearchOptionType = {
+  value: string;
+  label: string;
+};
 
-  const handleChange = async (input: string) => {
-    setValue(input);
-    const response: IGetRidersResponse = await getRidersByName(input);
-    if (response && response?.error) {
-      console.log('got error: ', response.error);
-    } else {
-      const riders = response?.riders ? [response.riders] : [];
-      const riderNames = riders.map(
-        (rider: IRiderInfo) => `${rider.name.first} ${rider.name.last}`
-      );
-      setData(riderNames);
-    }
+export default function Search() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [data, setData] = React.useState<SearchOptionType[]>([]);
+  const debouncedSearchValue = useDebounce(searchValue, 300); // 300ms debounce
+
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const search = async () => {
+      const response: IGetRidersResponse = await getRidersByName(debouncedSearchValue);
+      if (response && response?.error) {
+        console.log('got error: ', response.error);
+      } else {
+        const riders = Array.isArray(response?.riders) ? response?.riders : [];
+
+        const riderNames = riders.map((rider: IRiderInfo) => ({
+          value: String(rider.id),
+          label: `${rider.name.first} ${rider.name.last}`,
+        }));
+        setData(riderNames);
+      }
+    };
+    search();
+  }, [debouncedSearchValue]);
+
+  const handleChange = React.useCallback((input: string) => {
+    setSearchValue(input);
+  }, []);
+
+  const handleOptionSubmit = (option: string) => {
+    const riderUrl = `${APP_BASE_URL}${APP_RIDER_PATH}/${option}`;
+    router.push(riderUrl);
   };
+
   return (
     <Autocomplete
       leftSectionPointerEvents="none"
       leftSection={icon}
-      placeholder="Serch it bb"
+      placeholder="Search for Riders"
       data={data}
-      value={value}
+      limit={15}
+      value={searchValue}
       onChange={handleChange}
+      onOptionSubmit={handleOptionSubmit}
     />
   );
 }
