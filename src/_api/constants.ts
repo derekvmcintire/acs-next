2b51158bfa26bb4b6cbb175484ab314d975dbf1e
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-10-29 13:35:00 -0400

    Update base API URL (#66)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/_api/constants.ts
+++ src/_api/constants.ts
@@ -2,6 +2,6 @@ export const BASE_URL = 'http://localhost:8000';
 export const RACERS_PATH = '/racers';
 export const HISTORY_PATH = '/history';
 
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://localhost:8080/api/v1';
 export const API_RIDER_PATH = '/rider';
 export const API_RESULT_PATH = '/result';
