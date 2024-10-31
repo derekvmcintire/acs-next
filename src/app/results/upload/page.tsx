8b17d33a1b3714c1bc2ea45b7808da365bf22b63
Derek McIntire <derekbmcintire@gmail.com>
2024-10-31 18:16:09 -0400

    hold on buddy

---
+++ src/app/results/upload/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react'
+import { Text } from '@mantine/core';
+
+interface Component {
+
+}
+export default function Component() {
+    return (
+        <>
+        <Text>Upload Results Here</Text>
+        </>
+    )
+}
