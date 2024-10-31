4f93750618d355dc2ed9d385a61b665928678f13
Derek McIntire <derekbmcintire@gmail.com>
2024-10-31 18:16:45 -0400

    hello?

--- src/app/results/upload/page.tsx
+++ src/app/results/upload/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import React from 'react'
+import React from 'react';
 import { Text } from '@mantine/core';
 
-interface Component {
-
-}
+interface Component {}
 export default function Component() {
-    return (
-        <>
-        <Text>Upload Results Here</Text>
-        </>
-    )
+  return (
+    <>
+      <Text>Upload Results Here</Text>
+    </>
+  );
 }
