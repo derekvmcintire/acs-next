b73187f10af9a06b594bc36255f29f68afb9f8e1
Derek McIntire <derekbmcintire@gmail.com>
2024-11-01 21:07:36 -0400

    Add ability to use existing race

--- src/_processers/results/index.ts
+++ src/_processers/results/index.ts
@@ -1,5 +1,5 @@
 import { CreateResultReturnData } from '@/src/_api/create-result';
-import { PreparedResult, prepareResults } from '@/src/_processers/results/utility/prepare-results';
+import { parseResults, PreparedResult } from '@/src/_processers/results/utility/parse-results';
 import { ResultFormData } from '@/src/app/results/upload/page';
 import { createRace, CreateRaceReturnData } from '../../_api/create-race';
 import { processPreparedResult } from './utility/process-prepared-results';
@@ -9,8 +9,8 @@ export type ProcessResultsReturnData = {
   resultsCreated: CreateResultReturnData[];
 };
 
-export const processResults = async (data: ResultFormData): Promise<ProcessResultsReturnData> => {
-  const { name, startDate, location, results } = data;
+export const createRaceBeforeResults = async (data: ResultFormData) => {
+  const { name, startDate, location } = data;
   const raceData = {
     name,
     raceTypeId: 1,
@@ -20,17 +20,28 @@ export const processResults = async (data: ResultFormData): Promise<ProcessResul
   };
 
   const race = await createRace(raceData);
+  return race;
+};
+
+export const processResults = async (
+  existingRace: CreateRaceReturnData | undefined,
+  data: ResultFormData
+): Promise<ProcessResultsReturnData> => {
+  const race = existingRace || (await createRaceBeforeResults(data));
+
   if (!race || 'error' in race) {
     throw new Error(String('Error creating race'));
   }
+
+  const { results } = data;
 
   if (!results) {
     return Promise.resolve({ race, resultsCreated: [] });
   }
 
-  const preparedResults = prepareResults(results);
+  const parsedResults = parseResults(results);
   const finalizedResults = await Promise.all(
-    preparedResults.map(async (result: PreparedResult) => {
+    parsedResults.map(async (result: PreparedResult) => {
       const finalizedResult = await processPreparedResult(result, race);
       return finalizedResult;
     })
