24c6aeea1ee39fcb80b681aa7b6910ea671d13aa
Derek McIntire <derekbmcintire@gmail.com>
2024-11-06 09:19:16 -0500

    remove inline styles and add border radius to course map

--- src/_components/Race/Results/CourseMap/index.tsx
+++ src/_components/Race/Results/CourseMap/index.tsx
@@ -8,8 +8,9 @@ const DEFAULT_RWGPS_MAP = 'https://ridewithgps.com/embeds?type=route&id=44796948
 
 export default function CourseMap() {
   return (
-    <Container className={classes.courseMap}>
+    <Container className={classes.courseMapContainer}>
       <iframe
+        className={classes.courseMap}
         src={DEFAULT_RWGPS_MAP}
         style={{ width: '1px', minWidth: '100%', height: '525px', border: 'none' }}
         title="Ride with GPS Map"
