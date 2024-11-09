ce53f02e49c03bce3de739541400a7edac14059f
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-11-09 13:17:41 -0500

    T146 cleaning (#103)
    * refactor
    
    * Clean up ACSHome
    
    * make sure riders is never null
    
    * Refactor RecentRaces
    
    * pretty
    
    * Refactor Result Preview
    
    * Pull results preview list mapping out into own component
    
    * pretty
    
    * pretty
    
    * Refactor ResultsReport component
    
    * type name change
    
    * Refactor ResultsReportChart component
    
    * Refactor Winner Details
    
    * fix test
    
    * RiderTeams update
    
    * Update Form Instructions
    
    ---------
    
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/_processers/results/constants.ts
+++ src/_processers/results/constants.ts
@@ -2,7 +2,7 @@ export const VERIFIED_HEADERS = {
   category: ['category', 'div', 'division', 'cat', 'ctgry'],
   place: ['place', 'pl', 'p', 'pos', 'position', 'plc'],
   hometown: ['hometown', 'city', 'hmtwn', 'town'],
-  gap: ['gap'],
-  time: ['time'],
+  gap: ['gap', 'time gap'],
+  time: ['time', 'finish time', 'finishing time'],
   name: ['name', 'rider', 'bib & name'],
 } as const;
