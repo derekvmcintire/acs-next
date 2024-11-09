1ab0750e1218b72f7d5e64fcfdfed43d2c803203
Derek McIntire <derekbmcintire@gmail.com>
2024-11-09 09:42:56 -0500

    Refactor Result Preview

--- src/_components/Race/Results/ResultPreview/index.tsx
+++ src/_components/Race/Results/ResultPreview/index.tsx
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import React from 'react';
 import LabeledText from '@/src/_components/ui/LabeledText';
 import { RiderResult } from '@/src/_types/extended-types';
+import RemainingTopTen from './map-remaining-top-ten';
 import classes from '../../styles/race-results.module.css';
 
 interface ResultPreviewProps {
@@ -12,26 +13,24 @@ interface ResultPreviewProps {
 }
 
 export default function ResultPreview({ raceResults }: ResultPreviewProps) {
-  const firstResult = raceResults[0];
-  raceResults.shift();
+  if (raceResults.length === 0) return null;
+
+  const [firstResult, ...remainingResults] = raceResults;
   const { name, startDate, rider, location } = firstResult;
-  const winnerName = `${rider?.firstName} ${rider?.lastName}`;
+  const winnerName = rider ? `${rider?.firstName} ${rider?.lastName}` : 'Unknown Rider';
 
   return (
     <Container className={classes.resultsPreview}>
       <LabeledText isSpan label={dayjs(startDate).format('M/D')} text={name} />
       <Text fs="italic" span>{` - ${location}`}</Text>
-      <Spoiler maxHeight={5} showLabel="Show Top Ten" hideLabel="Hide Top Ten">
+      <Spoiler
+        maxHeight={5}
+        showLabel="Show Top Ten"
+        hideLabel="Hide Top Ten"
+        aria-expanded={false}
+      >
         <LabeledText color="orange" label="Winner" text={winnerName} />
-        {raceResults.map((result: RiderResult, i) => {
-          const riderResult = result.rider;
-          const riderName = `${riderResult?.firstName} ${riderResult?.lastName}`;
-          return (
-            <div key={result.place}>
-              <LabeledText hasColon={false} label={`${i + 2}.`} text={riderName} />
-            </div>
-          );
-        })}
+        <RemainingTopTen results={remainingResults} />
       </Spoiler>
     </Container>
   );
