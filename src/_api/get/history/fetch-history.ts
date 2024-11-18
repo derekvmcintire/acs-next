4be070e9db5192e901b142576ca269ef486c5c5a
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-11-17 21:11:01 -0500

    Update get rider history url (#127)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/_api/get/history/fetch-history.ts
+++ src/_api/get/history/fetch-history.ts
@@ -1,10 +1,10 @@
-import { API_BASE_URL, API_RESULT_PATH } from '@/src/_api/constants';
+import { API_BASE_URL, API_RESULT_PATH, API_RIDER_PATH } from '@/src/_api/constants';
 import { getResponse } from '../../helpers';
 import { IGetHistoryResponse } from '../../types';
 import { GetHistoryResponse } from './fetch-history-response-type';
 
 export const getRiderHistoryRequestUrl = (id: number) =>
-  `${API_BASE_URL}${API_RESULT_PATH}?riderId=${id}`;
+  `${API_BASE_URL}${API_RIDER_PATH}/${id}/${API_RESULT_PATH}`;
 
 export const fetchRiderHistory = async (id: number) => {
   const result = await getResponse(
