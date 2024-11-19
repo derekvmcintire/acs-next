286af3813503e608b4df23e1816a46dd69204f19
Derek McIntire <derekbmcintire@gmail.com>
2024-11-19 18:56:52 -0500

    Replace one instance of getResponse with simpleTsFetch

--- src/_api/get/races/fetch-races-totals.ts
+++ src/_api/get/races/fetch-races-totals.ts
@@ -1,5 +1,5 @@
+import { simpleTsFetch } from 'simple-fetch-ts';
 import { API_BASE_URL, API_RACES_PATH, API_TOTALS_PATH } from '@/src/_api/constants';
-import { simpleTypedFetch } from '../../helpers';
 import { IGetRacesTotalsResponse } from '../../types';
 import { GetRacesTotalsResponse, GetTotalsFilters } from './fetch-races-response-type';
 
@@ -21,7 +21,7 @@ export const getRacesTotalsUrl = (filters: GetTotalsFilters) => {
 export const fetchRacesTotals = async (
   filters: GetTotalsFilters
 ): Promise<IGetRacesTotalsResponse> => {
-  return simpleTypedFetch<GetRacesTotalsResponse[]>(getRacesTotalsUrl(filters))
+  return simpleTsFetch<GetRacesTotalsResponse[]>(getRacesTotalsUrl(filters))
     .then((response: GetRacesTotalsResponse[]) => {
       return { totals: response };
     })
