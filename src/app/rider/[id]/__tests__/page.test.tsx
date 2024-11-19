4b7b71ce968b12e2cdd99a4efa724cd095c6a2f4
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-11-19 13:02:45 -0500

    Use jest-vest package to mock fetch in jest tests (#129)
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

--- src/app/rider/[id]/__tests__/page.test.tsx
+++ src/app/rider/[id]/__tests__/page.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom/jest-globals';
 import '@testing-library/jest-dom';
+import { mockMultiGlobalFetch } from 'jest-vest';
 import { getRiderHistoryRequestUrl } from '@/src/_api/get/history/fetch-history';
 import { getRiderRequestUrl, getSingleRiderRequestUrl } from '@/src/_api/get/riders/fetch-rider';
 import { RIDER_INFO_TEST_ID } from '@/src/_components/Rider';
@@ -9,26 +10,10 @@ import { TOP_NAV_TEST_ID } from '@/src/_components/shared/TopNav';
 import { RIDER_SEARCH_DATA_TEST_ID } from '@/src/_components/shared/TopNav/Search';
 import { mockRacingHistory } from '@/src/_db/mock-data/mock-race-history';
 import { mockRider, mockTeamMembers, TEAM_B2C2_CONTES } from '@/src/_db/mock-data/mock-racer';
-import { mockMultiGlobalFetch, mockResponsePackage } from '@/src/_utility/test-helpers';
 import { render, screen } from '@/test-utils';
 import RiderPage from '../page';
 
 const mockId = 1;
-
-// set up mock responses for global.fetch
-const firstMockPackage: mockResponsePackage = {
-  expectedUrl: getRiderHistoryRequestUrl(mockId),
-  mockResponse: mockRacingHistory,
-};
-const secondMockPackage: mockResponsePackage = {
-  expectedUrl: getSingleRiderRequestUrl(mockId),
-  mockResponse: mockRider,
-};
-const thirdMockPackage: mockResponsePackage = {
-  expectedUrl: getRiderRequestUrl({ team: TEAM_B2C2_CONTES }),
-  mockResponse: mockTeamMembers,
-};
-const mockResultsPackages = [firstMockPackage, secondMockPackage, thirdMockPackage];
 
 jest.mock('../../../../_components/shared/TopNav/Search', () => {
   return function MockSearch() {
@@ -41,7 +26,21 @@ afterEach(() => {
 });
 
 beforeEach(() => {
-  mockMultiGlobalFetch(mockResultsPackages);
+  const mockResultSeeds = [
+    {
+      url: getRiderHistoryRequestUrl(mockId),
+      response: mockRacingHistory,
+    },
+    {
+      url: getSingleRiderRequestUrl(mockId),
+      response: mockRider,
+    },
+    {
+      url: getRiderRequestUrl({ team: TEAM_B2C2_CONTES }),
+      response: mockTeamMembers,
+    },
+  ];
+  mockMultiGlobalFetch(mockResultSeeds);
 });
 
 describe('RacerInfoServer', () => {
