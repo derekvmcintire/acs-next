857832c56d096abc1835e8863aeadeb7abe26b48
Derek McIntire <derekbmcintire@gmail.com>
2024-11-19 08:49:09 -0500

    Add cycling-results-parser package to replace parsing results

--- src/_processers/results/index.ts
+++ src/_processers/results/index.ts
@@ -1,10 +1,9 @@
+import { calculatePoints, parseResults, PreparedResult } from 'cycling-results-parser';
 import { GetRacesResponse } from '@/src/_api/get/races/fetch-races-response-type';
 import { CreateResultReturn } from '@/src/_api/post/results/create-result-return-type';
 import { RaceFormData } from '@/src/_components/Uploader/RaceForm/index';
-import { parseResults, PreparedResult } from '@/src/_processers/results/utility/parse-results';
 import { createRace } from '../../_api/post/races/create-race';
 import { processPreparedResult } from './utility/process-prepared-results';
-import { calculatePoints } from './utility/ranking-helper';
 
 export type ProcessResultsReturnData = {
   race: GetRacesResponse;
@@ -34,9 +33,10 @@ export const processResults = async (
   const totalRacers = parsedResults.length;
   const finalizedResults = await Promise.all(
     parsedResults.map(async (result: PreparedResult) => {
+      // @TODO figure out how to import types from library here
       const position = result?.place || 0;
       if (totalRacers && position) {
-        const points = calculatePoints(totalRacers, Number(position));
+        const points = calculatePoints({ totalRacers, position: Number(position) });
         result.points = points || 0;
       }
       const finalizedResult = await processPreparedResult(result, race, categories);
