6343b5d7cb8629c1919d656150340b0437c53237
Derek McIntire <derekbmcintire@gmail.com>
2024-11-21 14:10:15 -0500

    maybe problem files

--- src/_api/get/rankings/fetch-rankings.ts
+++ src/_api/get/rankings/fetch-rankings.ts
@@ -1,21 +1,35 @@
 import dayjs from 'dayjs';
-import { QueryParams, simple } from 'simple-fetch-ts';
 import { API_BASE_URL, API_RANKINGS_PATH, API_RIDER_PATH } from '@/src/_api/constants';
+import { getResponse } from '../../helpers';
 import { IGetRankingsResponse } from '../../types';
 import { GetRankingsResponse } from './fetch-rankings-response-type';
+
+export const getRankingsResultsRequestUrl = (year: number, limit?: number) => {
+  const baseUrl = `${API_BASE_URL}/${API_RIDER_PATH}/${API_RANKINGS_PATH}?year=${year}`;
+
+  return limit ? `${baseUrl}&limit=${limit}` : baseUrl;
+};
 
 type GetRankingsParams = {
   year?: number;
   limit?: number;
 };
 
-const url = `${API_BASE_URL}${API_RIDER_PATH}${API_RANKINGS_PATH}`;
-
 export const fetchRankings = async ({
   year,
   limit,
 }: GetRankingsParams): Promise<IGetRankingsResponse> => {
-  const params: QueryParams = { year: year || dayjs().year(), limit: limit || '' };
-  const response = await simple(url).params(params).fetch<GetRankingsResponse[]>();
-  return { rankings: response.data };
+  const result = await getResponse(
+    getRankingsResultsRequestUrl(year || dayjs().year(), limit),
+    async (response: Response): Promise<IGetRankingsResponse> => {
+      const parsedResponse: GetRankingsResponse[] = await response.json();
+      return { rankings: parsedResponse };
+    }
+  );
+
+  if ('error' in result) {
+    return { ...result, rankings: null };
+  }
+
+  return result;
 };
