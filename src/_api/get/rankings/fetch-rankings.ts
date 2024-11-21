4a9430adc87c8b6c0c70c1c51c034acedc9158c8
Derek McIntire <derekbmcintire@gmail.com>
2024-11-21 09:01:38 -0500

    Remove extra slashes in url

--- src/_api/get/rankings/fetch-rankings.ts
+++ src/_api/get/rankings/fetch-rankings.ts
@@ -4,18 +4,12 @@ import { API_BASE_URL, API_RANKINGS_PATH, API_RIDER_PATH } from '@/src/_api/cons
 import { IGetRankingsResponse } from '../../types';
 import { GetRankingsResponse } from './fetch-rankings-response-type';
 
-export const getRankingsResultsRequestUrl = (year: number, limit?: number) => {
-  const baseUrl = `${API_BASE_URL}/${API_RIDER_PATH}/${API_RANKINGS_PATH}?year=${year}`;
-
-  return limit ? `${baseUrl}&limit=${limit}` : baseUrl;
-};
-
 type GetRankingsParams = {
   year?: number;
   limit?: number;
 };
 
-const url = `${API_BASE_URL}/${API_RIDER_PATH}/${API_RANKINGS_PATH}`;
+const url = `${API_BASE_URL}${API_RIDER_PATH}${API_RANKINGS_PATH}`;
 
 export const fetchRankings = async ({
   year,
