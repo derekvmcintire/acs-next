51d257cc128629d91a896663fa1d6cab7fc84dcf
Derek McIntire <derekbmcintire@gmail.com>
2024-11-21 18:22:12 -0500

    cant pass nested objects as params

--- src/_api/get/races/fetch-races.ts
+++ src/_api/get/races/fetch-races.ts
@@ -36,6 +36,7 @@ const _buildParams = (filters: GetRacesFilters): QueryParams => {
 export const fetchRaces = async (filters: GetRacesFilters): Promise<IGetRacesResponse> => {
   const url = `${API_BASE_URL}${API_RACES_PATH}`;
   const params = _buildParams(filters);
-  const response = await simple(url).params(params).fetch<GetRacesResponse[]>();
+  const lowerCaseKeys = true;
+  const response = await simple(url).params(params, lowerCaseKeys).fetch<GetRacesResponse[]>();
   return { races: response.data };
 };
