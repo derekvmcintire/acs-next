5b61a82a15dd4f18c48475f07668cee16576eab2
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2024-11-23 20:17:18 -0500

    Switch to New Process Results Endpoint (#137)
    * Update process results logic
    
    * add return type to createRaceResults
    
    * remove process results
    
    ---------
    
    Co-authored-by: Derek McIntire <derekbmcintire@gmail.com>

---
+++ src/_api/post/races/results/create-race-results.ts
@@ -0,0 +1,16 @@
+import { simple } from 'simple-fetch-ts';
+import { API_BASE_URL, API_RACES_PATH, API_RESULT_PATH } from '@/src/_api/constants';
+import { CreateRaceResultsRequest } from './create-race-results-request-type';
+import { CreateRaceResultsReturn } from './create-race-results-return-type';
+
+export const url = `${API_BASE_URL}${API_RACES_PATH}${API_RESULT_PATH}`;
+
+export const createRaceResults = async (
+  requestData: CreateRaceResultsRequest
+): Promise<CreateRaceResultsReturn> => {
+  const response = await simple(url)
+    .body<CreateRaceResultsRequest>(requestData)
+    .post<CreateRaceResultsReturn>();
+
+  return response.data;
+};
--- src/_processers/results/index.ts
+++ src/_processers/results/index.ts
@@ -1,9 +1,7 @@
-import { calculatePoints, parseResults, PreparedResult } from 'cycling-results-parser';
 import { GetRacesResponse } from '@/src/_api/get/races/fetch-races-response-type';
 import { CreateResultReturn } from '@/src/_api/post/results/create-result-return-type';
 import { RaceFormData } from '@/src/_components/Uploader/RaceForm/index';
 import { createRace } from '../../_api/post/races/create-race';
-import { processPreparedResult } from './utility/process-prepared-results';
 
 export type ProcessResultsReturnData = {
   race: GetRacesResponse;
@@ -22,26 +20,4 @@ export const createRaceBeforeResults = async (data: RaceFormData) => {
 
   const race = await createRace(raceData);
   return race;
-};
-
-export const processResults = async (
-  race: GetRacesResponse,
-  results: string,
-  categories: string[]
-): Promise<ProcessResultsReturnData> => {
-  const parsedResults = parseResults(results);
-  const totalRacers = parsedResults.length;
-  const finalizedResults = await Promise.all(
-    parsedResults.map(async (result: PreparedResult) => {
-      const position = result?.place || 0;
-      if (totalRacers && position) {
-        const points = calculatePoints({ totalRacers, position: Number(position) });
-        result.points = points || 0;
-      }
-      const finalizedResult = await processPreparedResult(result, race, categories);
-      return finalizedResult;
-    })
-  );
-
-  return Promise.resolve({ race, resultsCreated: finalizedResults });
 };
